ef28e14ece97beea9fe8fb50b9a2585dc5143ae3
awpmbakker@gmail.com <awpmbakker@gmail.com>
2026-02-10 17:24:03 +0000

    Edited settings and Validation

--- client_code/Validation.py
+++ client_code/Validation.py
@@ -1,30 +1,14 @@
-import anvil.server
-import anvil.users
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
-# This is a module.
-# You can define variables and functions here, and use them from any form. For example, in a top-level form:
-#
-#    from .. import Module1
-#
-#    Module1.say_hello()
-#
-def say_hello():
-  print("Hello, world")
-
-
 import datetime
 import re
 import time
 
 from anvil import *
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
-import anvil.users
+#import anvil.google.auth, anvil.google.drive
+#from anvil.google.drive import app_files
+#import anvil.tables as tables
+#import anvil.tables.query as q
+#from anvil.tables import app_tables
+#import anvil.users
 
 from .popover import popover, pop
 
